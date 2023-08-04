4ba6b3da015d1f41fcd800c82889f7daf9d348c2
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2023-08-04 19:36:57 +0500

    Get API

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ def getData(request):
     person = [
         {'name':'Dennis', 'age':28},
         {'name':'Jhon', 'age':20},
-        {'name':'Dennis', 'age':28},
-        {'name':'Jhon', 'age':20},
+        {'name':'Danna', 'age':23},
+        {'name':'Jhosa', 'age':25},
     ]
     return Response(person)
